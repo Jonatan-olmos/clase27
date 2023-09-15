52c3c29aa8d41f6f77bfa651cbf945f22f87c7a6
jonatan olmos <jon.ol.23.joo@gmail.com>
2023-09-15 10:19:25 -0300

    swith

---
+++ app.js
@@ -0,0 +1,20 @@
+// todo: hace ek front de la calculadora 
+const process = require ('process');
+const {argv} = process;
+
+function calculadora (comando ) {
+    
+switch (comando ) {
+    case "sumnar ":
+        
+        break;
+
+    default:
+        return "comando no valido"
+        break;
+}
+}
+
+
+console.log(calculadora(argv[2]
+, argv[3]));
